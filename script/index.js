3fe404059fda42adfeb3c3f219d3b052541420f7
AnaKhotornichan <110341153+AnaKhotornichan@users.noreply.github.com>
2022-11-27 22:03:06 +0200

    Homework4

--- script/index.js
+++ script/index.js
@@ -1,11 +1,5 @@
-const firstNumber = prompt("Please, enter a number!");
-const secondNumber = prompt("Please, enter a number!");
-const sum = parseInt(firstNumber) + parseInt(secondNumber);
-const sub = parseInt(firstNumber) - parseInt(secondNumber);
-const mul = parseInt(firstNumber) * parseInt(secondNumber);
-const div = parseInt(firstNumber) / parseInt(secondNumber);
-const alertMsg = firstNumber + '+' + secondNumber + '=' + sum + '\n' +
-firstNumber + '-' + secondNumber + "=" + sub + '\n' + 
-firstNumber + '*' + secondNumber + "=" + mul + '\n' +
-firstNumber + "/" + secondNumber + "=" + div; 
+const firstNumber = +prompt("Please, enter a number!");
+const secondNumber = +prompt("Please, enter a number!");
+const thirdNumber = +prompt("Please, enter a number!");
+const alertMsg = (firstNumber + secondNumber + thirdNumber) / 3;
 alert (alertMsg);
