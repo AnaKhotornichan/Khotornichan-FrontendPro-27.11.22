11627a4e17a0127ea1148b96d112e6476cc7dc7e
AnaKhotornichan <110341153+AnaKhotornichan@users.noreply.github.com>
2022-12-01 00:02:04 +0200

    added hw6

--- script/index.js
+++ script/index.js
@@ -1,54 +1,14 @@
-const year = prompt("What is your year of birth?");
-let age = 2022 - parseInt(year);
-if (!year) {
-    alert("It's a pitty you didn't want to tell us your age!");
-    age = "-";
-}
-
-const city = prompt("Which city do you live in?");
-let cityAnswer;
-if (!city) {
-    alert("It's a pitty you didn't want to tell us where you are from!");
-}
-
-const sport = prompt("What is your favourite sport?");
-let sportAnswer;
-if (!sport) {
-    alert("It's a pitty you didn't want to tell us where your favourite sport!");
-    sportAnswer = "-";
-}
-
-
-switch (city) {
-    case "Kyiv":
-        cityAnswer = "You live in the capital of Ukraine!";
+let numOrStr = prompt('input number or string');
+switch(true) {
+    case numOrStr === null: 
+        console.log('ви скасували');
         break;
-    case "Washington":
-        cityAnswer = "You live in the capital of USA!";
+    case numOrStr.trim() === '': 
+        console.log('Empty String');
         break;
-    case "London":
-        cityAnswer = "You live in the capital of Great Britain!";
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
         break;
     default:
-        cityAnswer = "You live in the city of " + city + "!";
+        console.log('OK!');
 }
-
-if (!city) {
-    cityAnswer = "-";
-}
-
-
-if (sport === "basketball") {
-    sportAnswer = "Nice! Do you want to be the next Michael Jordan?";
-} else if (sport === "football") {
-    sportAnswer = "Nice! Do you want to be the next Lionel Messi?";
-} else if (sport === "golf") {
-    sportAnswer = "Nice! Do you want to be the next Tiger Woods?";
-} else {
-    sportAnswer = sport;
-}
-
-const alertMsg = age + "\n" + 
-cityAnswer + "\n" 
-+ sportAnswer;
-alert(alertMsg);
