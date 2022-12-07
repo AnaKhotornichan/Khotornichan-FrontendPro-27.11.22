728c39ae40e80696eaf4ec1559c551f0862461c7
AnaKhotornichan <110341153+AnaKhotornichan@users.noreply.github.com>
2022-12-07 17:28:38 +0200

    added hw9

--- script/index.js
+++ script/index.js
@@ -1,97 +1,61 @@
 /* Exercise 1 */
-const digitOne = prompt("Enter first number");
-const digitTwo = prompt("Enter second number");
 
-if (+digitOne > +digitTwo) {
-    alert ("First number is grater than the second");
-} else {
-    alert ("Second number is greater than first")
+let primaryNumber = 20;
+let alertMsg = "";
+while (primaryNumber <= 30) {
+    alertMsg = alertMsg + primaryNumber + " ";
+    primaryNumber = primaryNumber + 0.5;
 }
+alert(alertMsg);
 
 
 /* Exercise 2 */
-const km = prompt("Please, enter lenght in kilometers")
-const ft = prompt("Please, enter length in feet")
 
-const ftLength = parseInt(ft) / 3280.84;
-
-if (+km < ftLength) {
-    alert ("Lenght in kilometers is lesser than in feet");
-} else {
-    alert ("Lenght in feet is lesser than in kilometers")
+let usd = 10;
+const uahRate = 27;
+let alertMsg= "";
+while (usd <= 100) {
+    alertMsg = alertMsg + (usd * uahRate) + " ";
+    usd = usd + 10;
 }
+alert(alertMsg);
 
 
 /* Exercise 3 */
-const numberA = prompt("Enter number A");
-const numberB = prompt("Enter number B");
 
-if (+numberA % +numberB === 0) {
-    alert ("Number A can be diveded by number B");
-} else {
-    alert ("Number A can't be diveded by number B");
+let numberN = 50;
+let alertMsg = "";
+for (let i = 1; i <= 100; i++) {
+    let square = i * i;
+    if (square > numberN) break;
+    alertMsg = alertMsg + i + " ";
 }
+alert(alertMsg);
 
 
 /* Exercise 4 */
-let num = 10;
-while (num < 21) {
-    const comma = num === 20 ? '' : ','; 
-    document.write(num + comma);
-    num++;
+
+let num = 6;
+let primeNumber = "This is a prime number!";
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+        primeNumber = "This is not a prime number!";
+        break;
+    }
 }
+console.log(primeNumber);
 
 
 /* Exercise 5 */
-let num = 10;
-while (num <= 20) {
-    const comma = num === 20 ? '' : ','; 
-    document.write(Math.pow(num, 2) + comma);
-    num++;
-}
 
-
-/* Exercise 6 */
-for (let num = 1; num <=10; num++) {
-    const comma = num === 10 ? '' : ', ';
-    document.write(num + " * 7" + " = " + (num * 7) + comma);
-}
-
-
-/* Exercise 7 */
-let num = 1;
-let result = 0;
-while (num <= 500) {
-    result += num;
-    num++;
-}
-alert(result/500);
-
-
-/* Exercise 8 */
-let num = 15;
-let product = 1;
-while (num <=35) {
-    product = product * num;
-    num++;
-}
-alert(product);
-
-
-/* Exercise 9 */
-let sum = 0;
-for (let num = 30; num <=80; num = num + 2) {
-    sum = sum + num;
-}
-alert(sum);
-
-
-/* Exercise 10 */
-let mult = "";
-for (let num = 100; num <=200; num++) {
-    const comma = num === 198 ? '' : ',';
-    if (num % 3 === 0) {
-        mult = mult + num + comma;
+let num = 27;
+let result = "You can't get this number by exponentiation of the number 3!";
+for (let i = 2; i < num; i++) {
+    if (Math.pow(3, i) === num) {
+        result = "You can get this number by exponentiation of the number 3!";
+        break;
     }
 }
-alert(mult);
+console.log(result);
+
+
