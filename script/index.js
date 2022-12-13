ba316868da965d4187fb474c22d494fc5d6cd589
AnaKhotornichan <110341153+AnaKhotornichan@users.noreply.github.com>
2022-12-13 21:01:34 +0200

    added hw11

--- script/index.js
+++ script/index.js
@@ -1,22 +1,51 @@
-const arrLength = +prompt("Please, enter an array lenght!");
-const arr = [];
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-for (let i = 1; i <= arrLength; i++) {
-    let arrEl = +prompt("Please, enter a number!");
-    arr.push(arrEl);
-    console.log(arr);
-}
+const minNumber = Math.min(...arr);
+console.log(minNumber);
+const indexMin = arr.indexOf(minNumber);
+console.log(indexMin);
 
-const arrSorted = arr.sort((firstEl, secondEl) => {
-    if (firstEl > secondEl) {
-        return 1;
-    } else if (firstEl < secondEl) {
-        return -1;
-    }
-    return 0;
+const maxNumber = Math.max(...arr);
+console.log(maxNumber);
+const indexMax = arr.indexOf(maxNumber);
+console.log(indexMax);
+
+const negativeNumbers = arr.filter(number => number < 0);
+console.log(negativeNumbers.length);
+
+const odds = [];
+arr.forEach((number) => {
+  if (number % 2 !== 0 && number > 0) {
+    odds.push(number);
+  }
 });
+console.log(odds.length);
 
-console.log(arrSorted);
+const even = [];
+arr.forEach((number) => {
+  if (number % 2 === 0 && number > 0) {
+    even.push(number);
+  }
+});
+console.log(even.length);
 
-arrSorted.splice(1,3);
-console.log (arrSorted);
+let sumEven = 0;
+for (let i = 0; i < even.length; i++) {
+    sumEven += even[i];
+  }
+console.log(sumEven);
+
+let sumOdds = 0;
+for (let i = 0; i < odds.length; i++) {
+    sumOdds += odds[i];
+  }
+console.log(sumOdds);
+
+let multEven = 1;
+for (let i = 0; i < even.length; i++) {
+    multEven *= even[i];
+  }
+console.log(multEven);
+
+const resetArr = arr.map((number, i) => indexMax !== i ? 0 : number);
+console.log(resetArr);
